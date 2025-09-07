646175a11460ea4a806c7ec7c982dd2cab154d72
IndoPhoenix <vigyanavart@gmail.com>
2025-09-07 23:46:56 +0530

    some changes 2

--- omicron-client/.next/server/server-reference-manifest.js
+++ omicron-client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"a/eBycFvmQ85AHSfuT1eyYSZtFIUP0nJ7pVBTqBqQFY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
